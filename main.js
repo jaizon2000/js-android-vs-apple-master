246f34c0e9eb5f02a6a6076ea146db28bb0e7cb6
Jehdi Aizon <46204336+jaizon2000@users.noreply.github.com>
2019-07-05 08:30:56 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+document.getElementById('android-btn').addEventListener('click', getAndroid);
+document.getElementById('apple-btn').addEventListener('click', getApple);
+//  Functions
+function getAndroid() {
+    // Image
+    document.getElementById('logo').src = 'images/Android-Logo.jpg';
+    // Button Text
+    document.getElementById('linkBtn').innerHTML = 'Android Home';
+    // Button Link
+    document.getElementById('linkBtn').href = 'https://www.android.com/';
+    //Button Color
+    document.getElementById('linkBtn').style.backgroundColor = '#a4c93b';
+    // Body Background Color
+    document.body.style.backgroundColor = '#a4c93b';
+}
+function getApple() {
+    // Image
+    document.getElementById('logo').src = 'images/Apple-Logo.jpg';
+    // Button Text
+    document.getElementById('linkBtn').innerHTML = 'Apple Home';
+    // Button Link
+    document.getElementById('linkBtn').href = 'https://www.apple.com/';
+    //Button Color
+    document.getElementById('linkBtn').style.backgroundColor = '#b6bcca';
+    // Body Background Color
+    document.body.style.backgroundColor = '#b6bcca';
+}
